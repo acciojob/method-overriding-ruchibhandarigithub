8900d75d4067c0dd5ca9d6474717a4b6f8224ff4
ruchibhandarigithub <bhandariruchi498@gmail.com>
2023-01-14 00:03:07 -0800

    fourth commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,22 @@
 package com.driver;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         B a = new B();
-       System.out.println(a.meth());
+        System.out.println(a.meth());
+        System.out.println(a.meth());
 
     }
-  
+
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
